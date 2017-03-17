b97b694928e2a6566b838680c75e884942cd0d91
Daniel Song <xdtsong@gmail.com>
2017-03-16 21:44:10 -0700

    Add support for resource versioning in Employee

--- src/main/java/com/teradata/payroll/Employee.java
+++ src/main/java/com/teradata/payroll/Employee.java
@@ -1,9 +1,11 @@
 package com.teradata.payroll;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Version;
 
 @Data
 @Entity
@@ -13,6 +15,8 @@ public class Employee {
     private String firstName;
     private String lastName;
     private String description;
+
+    private @Version @JsonIgnore Long version;
 
     private Employee() {}
 
